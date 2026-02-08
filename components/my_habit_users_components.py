20bb17aa00fae57fe32d38fb01ed3e616d1c7a7e
Valentayne <yehoianvb0510@gmail.com>
2026-02-08 16:28:28 +0200

    add my_habit, my_habit_users components. add one_habit_page

---
+++ components/my_habit_users_components.py
@@ -0,0 +1,14 @@
+""" Habit components """
+from selenium.webdriver.common.by import By
+
+from components.base_component import BaseComponent
+from utils.types import Locators
+
+
+class MyHabitUsersComponents(BaseComponent):#FriendAbstract required
+    """ Habit user components """
+    stars: Locators = (By.CSS_SELECTOR, ".habit-header .stars")
+    tags: Locators = (By.CSS_SELECTOR, ".habit-info .tags")
+    all_titles: Locators = (By.XPATH, "//app-habits-gallery-view//h2")
+    more_buttons:Locators = (By.CSS_SELECTOR, ".habit-action-wrp .secondary-global-button")
+    added_habit_button:Locators = (By.CSS_SELECTOR,".habit-action-wrp .primary-global-button")
