5e97f8e7e5271513c5c2285c821d31676421b458
kettar1ec <glebshramko5487@gmail.com>
2026-02-10 23:01:01 +0200

    added buttons

--- pages/edit_profile_page.py
+++ pages/edit_profile_page.py
@@ -17,6 +17,8 @@ class ProfileEditPage(BasePage):
 
     page_root_locator: Locators = (By.CLASS_NAME, "edit_prof-wrap")
     page_title_locator: Locators = (By.TAG_NAME, "h2")
+    cancel_button_locator = (By.CSS_SELECTOR, ".buttons .secondary-global-button"    )
+    save_button_locator = (By.CSS_SELECTOR, ".buttons .primary-global-button")
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
@@ -27,6 +29,8 @@ class ProfileEditPage(BasePage):
         self.social_networks: SocialNetworksComponent = SocialNetworksComponent(page_root)
         self.profile_privacy: ProfilePrivacyComponent = ProfilePrivacyComponent(page_root)
         self.email_preferences: EmailPreferencesComponent = EmailPreferencesComponent(page_root)
+        self.cancel_button: WebElement = page_root.find_element(*self.cancel_button_locator)
+        self.save_button: WebElement = page_root.find_element(*self.save_button_locator)
 
     def get_page_header(self) -> str:
         """Return page title text."""
@@ -47,3 +51,15 @@ class ProfileEditPage(BasePage):
     def get_email_preferences_component(self) -> EmailPreferencesComponent:
         """Return EmailPreferencesComponent instance."""
         return self.email_preferences
+
+    def click_cancel(self):
+        """Click Cancel button."""
+        self.cancel_button.click()
+
+    def click_save(self):
+        """Click Save button."""
+        self.save_button.click()
+
+    def is_save_enabled(self) -> bool:
+        """Check if Save button is enabled."""
+        return self.save_button.is_enabled()
