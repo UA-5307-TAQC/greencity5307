8c4c405a6d55486cbbb24078264554ff7158f286
Valentayne <yehoianvb0510@gmail.com>
2026-02-11 21:43:08 +0200

    remake code

--- pages/my_habit_page.py
+++ pages/my_habit_page.py
@@ -7,7 +7,7 @@ from components.base_component import BaseComponent
 from utils.types import Locators
 
 
-class MyHabitComponent(BaseComponent): # MySpaceAbstract required
+class MyHabitPage(BaseComponent): # MySpaceAbstract required
     """ Habit component """
     habit_cards_list: Locators = (By.TAG_NAME, "app-one-habit")
     add_new_habit_button: Locators = (By.XPATH, "//*[@id='create-button-new-habit']/span")
