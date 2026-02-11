3718cc6b6aa1d805444a12168c13b009eb9b3c64
Anzhela <anzhela.en.mal@gmail.com>
2026-02-11 22:40:50 +0200

    formating

--- components/header_component.py
+++ components/header_component.py
@@ -4,7 +4,6 @@
 from __future__ import annotations
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -16,9 +15,6 @@ class HeaderComponent(BaseComponent):
     """Component class for the header section of a web page."""
     new_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/news']")
     event_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/events']")
-
-    def __init__(self, root: WebElement):
-        super().__init__(root)
 
     def click_new_link(self) -> "EcoNewsPage":
         """Click the news link in the header and return an instance of the EcoNewsPage."""
