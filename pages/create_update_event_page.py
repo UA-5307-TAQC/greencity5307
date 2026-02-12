bb48a26d64932ab876d7064140bce9ea25d3574e
kettar1ec <glebshramko5487@gmail.com>
2026-02-12 10:43:20 +0200

    added data_time_component.py

--- pages/create_update_event_page.py
+++ pages/create_update_event_page.py
@@ -6,6 +6,7 @@ from components.event_header_component import EventHeaderComponent
 from components.chip_set_component import ChipSetComponent
 from components.event_type_component import EventTypeComponent
 from components.quill_editor_component import QuillEditorComponent
+from components.data_time_component import DateTimeComponent
 
 from utils.types import Locators
 
@@ -16,17 +17,26 @@ class CreateEventPage(BasePage):
     chip_set_root: Locators = (By.CSS_SELECTOR, "div.mdc-evolution-chip-set__chips")
     event_type_root: Locators = (By.CSS_SELECTOR, "div.event-type-wrapper")
 
-
     def __init__(self, driver):
         super().__init__(driver)
+
         h_root = self.driver.find_element(*self.header_root)
         self.event_header = EventHeaderComponent(h_root)
+
         chip_root = self.driver.find_element(*self.chip_set_root)
         self.chip_set = ChipSetComponent(chip_root)
+
         type_root = self.driver.find_element(*self.event_type_root)
         self.event_type = EventTypeComponent(type_root, self.driver)
+
         description_root = self.driver.find_element(
             By.CSS_SELECTOR,
             "quill-editor[formcontrolname='description']"
         )
         self.description = QuillEditorComponent(description_root)
+
+        date_time_root = self.driver.find_element(
+            By.CSS_SELECTOR,
+            "div.date-time"
+        )
+        self.date_time = DateTimeComponent(date_time_root)
