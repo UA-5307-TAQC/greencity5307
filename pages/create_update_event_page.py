07ad0512295e43dbec6e6ec5b1a1e9c34a0cc1d8
kettar1ec <glebshramko5487@gmail.com>
2026-02-12 09:41:05 +0200

    added chip_set_component.py

--- pages/create_update_event_page.py
+++ pages/create_update_event_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
 from components.event_header_component import EventHeaderComponent
+from components.chip_set_component import ChipSetComponent
 
 from utils.types import Locators
 
@@ -10,8 +11,11 @@ from utils.types import Locators
 class CreateEventPage(BasePage):
     """Create event page."""
     header_root: Locators = (By.CSS_SELECTOR, "div.header-container")
+    chip_set_root: Locators = (By.CSS_SELECTOR, "div.mdc-evolution-chip-set__chips")
 
     def __init__(self, driver):
         super().__init__(driver)
         root = self.driver.find_element(*self.header_root)
-        self.header = EventHeaderComponent(root)
+        self.event_header = EventHeaderComponent(root)
+        chip_root = self.driver.find_element(*self.chip_set_root)
+        self.chip_set = ChipSetComponent(chip_root)
