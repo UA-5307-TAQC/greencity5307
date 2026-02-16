b7a5c3bba399d33a468afc45034ab6b8b2e820a8
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-02-16 21:02:12 +0200

    "Addition of test"

--- tests/test_createupdateeconewspage.py
+++ tests/test_createupdateeconewspage.py
@@ -1,35 +1,53 @@
 """This module contains test Create eco news with valid data"""
 import pytest
+import allure
 from pages.create_update_eco_news_page import CreateUpdateEcoNewsPage
-
-
 @pytest.mark.regression
-def test_create_eco_news_with_valid_data(driver):
+class TestCreateUpdateEcoNewsPage:
     """
-    TC-101
-    Title: Create eco news with valid data
-    Author: Vitalina Kliuieva
-    Priority: High
+        TC-101
+        Title: Create eco news with valid data
+        Author: Vitalina Kliuieva
+        Priority: High
     """
 
-    page = CreateUpdateEcoNewsPage(driver)
+    @allure.title("Test Validation: Create Eco News with Valid Data")
+    @allure.description(
+        "This test verifies that a user can successfully create eco news with valid data. ")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.label("owner", "vitalina.kliuieva")
+    @allure.testcase("TC-101")
+    @allure.step("Open Create Eco News page")
+    def test_open_create_update_eco_news_page(self, driver):
+        """Open Create Eco News page."""
+        page = CreateUpdateEcoNewsPage(driver)
 
-    assert page.is_page_opened(), "Create Eco News page is not opened"
+        assert page.is_page_opened(), "Create Eco News page is not opened"
 
-    form = page.get_form()
+    @allure.step("Fill in the form with valid data")
+    def test_fill_in_form(self, driver):
+        """Fill in the form with valid data."""
+        page = CreateUpdateEcoNewsPage(driver)
 
-    title = "Save the Planet"
-    tags = ("Events", "News")
-    source = "Green Blog"
-    content = "Eco content " * 30
-    image_path = "tests/data/test_image.jpg"
+        form = page.get_form()
 
-    form.fill_form(
-        title=title,
-        tags=tags,
-        source=source,
-        content=content,
-        image_path=image_path
-    )
+        title = "Save the Planet"
+        tags = ("Events", "News")
+        source = "Green Blog"
+        content = "Eco content " * 30
+        image_path = "tests/data/test_image.jpg"
 
-    page.click_submit()
+        form.fill_form(
+            title=title,
+            tags=tags,
+            source=source,
+            content=content,
+            image_path=image_path
+        )
+
+    @allure.step("Submit the form")
+    def test_submit_form(self, driver):
+        """Submit the form."""
+        page = CreateUpdateEcoNewsPage(driver)
+
+        page.click_submit()
