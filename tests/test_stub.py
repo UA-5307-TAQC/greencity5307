db6223d69c88de10d05d8a658d567b558b35427f
Liubomyr Halamaha <galamagal86@gmail.com>
2026-02-16 12:25:05 +0200

    remove all

--- tests/test_stub.py
+++
@@ -1,4 +0,0 @@
-"""A stub test file to ensure the testing framework is set up correctly."""
-def test_nothing():
-    """A test that does nothing and always passes."""
-    assert True
--- utils/types.py
+++
@@ -1,6 +0,0 @@
-"""This module defines custom type aliases for commonly used data structures
-in Selenium-based web automation."""
-
-from selenium.webdriver.common.by import By
-
-Locators = tuple[By, str]
