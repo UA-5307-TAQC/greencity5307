2cfd2622437b14e6499e7f5f12abe41fe762c181
D1xiS <oktaviosilva96@gmail.com>
2026-02-17 18:53:25 +0200

    Added allure descriptions, steps, etc.

--- tests/ui/test_change_language.py
+++ tests/ui/test_change_language.py
@@ -3,18 +3,30 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.main_page import MainPage
 
-@allure.title('Change language test for main page.')
+@allure.epic("GreenCity Main Functionality")
+@allure.feature("Localization")
+@allure.story("User can switch site language from Header")
+@allure.testcase("TC-BP-001") # ID тест-кейсу
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title("Verify language switching (UA <-> EN) in Header")
 def test_change_language(driver: WebDriver):
     """Change language test for main page."""
     main_page = MainPage(driver)
 
-    if main_page.header.get_new_link_text() != "Еко Новини":
+    with allure.step("Pre-condition: Ensure site is loaded in Ukrainian (UA)"):
+        if main_page.header.get_new_link_text() != "Еко Новини":
+            main_page.header.switch_language_to("ua")
+
+        assert main_page.header.get_new_link_text() == "Еко Новини", "Text is not in Ukrainian."
+
+    with allure.step("Step 1: Switch language to English (EN)"):
+        main_page.header.switch_language_to("en")
+
+    with allure.step("Step 2: Verify Navigation Menu text is translated to 'Eco News'"):
+        assert main_page.header.get_new_link_text() == "Eco News", "Text did not switch to English."
+
+    with allure.step("Step 3: Switch language back to Ukrainian (UA)"):
         main_page.header.switch_language_to("ua")
 
-    assert main_page.header.get_new_link_text() == "Еко Новини", "Text is not in Ukrainian."
-
-    main_page.header.switch_language_to("en")
-    assert main_page.header.get_new_link_text() == "Eco News", "Text did not switch to English."
-
-    main_page.header.switch_language_to("ua")
-    assert main_page.header.get_new_link_text() == "Еко Новини", "Text did not switch back to Ukrainian ."
+    with allure.step("Step 4: Verify Navigation Menu text reverted to 'Еко Новини'"):
+        assert main_page.header.get_new_link_text() == "Еко Новини", "Text did not switch back to Ukrainian ."
