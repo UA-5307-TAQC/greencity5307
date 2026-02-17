e88dade752fa1f79bd5a41fb5dccd8f06af3e010
Valentayne <yehoianvb0510@gmail.com>
2026-02-17 15:07:37 +0200

    work with debug mod

--- pages/one_habit_page.py
+++ pages/one_habit_page.py
@@ -73,29 +73,14 @@ class OneHabitPage(BasePage):
         self.click(self.to_do_list_edit_button)
         return self
 
-    # def add_element_into_list(self, message: str):
-    #     """write element in list and add it"""
-    #     self.find(self.custom_item_input).send_keys(message)
-    #     WebDriverWait(self.driver, 10).until(
-    #         EC.element_to_be_clickable(self.custom_item_add)
-    #     ).click()
-    #     WebDriverWait(self.driver, 10).until(
-    #         EC.text_to_be_present_in_element(self.list_items_loc, message)
-    #     )
-    #     return self
-
     def add_element_into_list(self, message: str):
         """write element in list and add it"""
         self.find(self.custom_item_input).send_keys(message)
-
-        # Клік по кнопці додавання
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(self.custom_item_add)
         ).click()
-
-        new_item_locator = (By.XPATH, f"//*[contains(text(), '{message}')]")
         WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(new_item_locator)
+            EC.text_to_be_present_in_element(self.list_items_loc, message)
         )
         return self
 
@@ -107,21 +92,7 @@ class OneHabitPage(BasePage):
         )
         return self
 
-    # def check_added_element(self):
-    #     """check added element"""
-    #     saved_items = self.wait.until(EC.presence_of_all_elements_located(self.list_items_loc))
-    #     return [item.text.strip() for item in saved_items]
-    def check_added_element(self, expected_text):
-        """check added element with explicit wait for the text"""
-
-        specific_item_loc = (By.XPATH,
-                             f"//app-habit-edit-to-do-list//li//span[contains(text(), "
-                             f"'{expected_text}')]")
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(specific_item_loc),
-            message=f"Елемент '{expected_text}' не з'явився у списку після збереження!")
-
-
-        # Тепер можна безпечно повертати весь список
+    def check_added_element(self):
+        """check added element"""
         saved_items = self.wait.until(EC.presence_of_all_elements_located(self.list_items_loc))
         return [item.text.strip() for item in saved_items]
--- tests/ui/test_one_event_page_add_custom_item_in_to_do_list.py
+++ tests/ui/test_one_event_page_add_custom_item_in_to_do_list.py
@@ -30,6 +30,6 @@ def test_one_event_page_add_custom_item_in_to_do_list(driver: WebDriver):
 
     one_habit_page.save_element()
 
-    items = one_habit_page.check_added_element(new_item_text)
+    items = one_habit_page.check_added_element()
 
     assert new_item_text  in items, "Текст не зберігся!"
