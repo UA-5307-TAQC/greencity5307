3609b94ea36cfdbbd8138d84a373ca6943e4c447
Kliuieva Vitalina <71712556+VitalinaKliuieva@users.noreply.github.com>
2026-02-19 13:59:26 +0200

    "Addition of create_update_eco_news_page.py, creation of components and test" (#78)
    * "Addition of create_update_eco_news_page.py, creation of components and test"
    
    * "Addition of test"
    
    * "Addition of pages"
    
    * "Editing of test to pass(ignore of picture input)"
    
    * "Editing of test to pass(ignore of picture input)"
    
    * "Editing of test to pass(ignore of picture input)"
    
    * "Editing of test to pass(ignore of picture input)"
    
    * "Editing of test to pass(ignore of picture input)"
    
    ---------
    
    Co-authored-by: Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
    Co-authored-by: Liubomyr Halamaha <galamagal86@gmail.com>

--- components/header_component.py
+++ components/header_component.py
@@ -9,32 +9,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from components.base_component import BaseComponent
-from components.common_components.auth_components.signin_modal_component \
-    import SignInComponent
-from utils.types import Locators
+from components.common_components.auth_components.signin_modal_component import SignInComponent
 from utils.logger import logger
+from utils.types import Locators
 
 
 class HeaderComponent(BaseComponent):
     """Component class for the header section of a web page."""
     new_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/news']")
-    event_link_locator: Locators = (By.XPATH,
-                                    ".//a[@href='#/greenCity/events']")
+    event_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/events']")
     sign_in_link_locator: Locators = (By.CSS_SELECTOR,
                                       ".header_navigation-menu-right-list > .header_sign-in-link")
 
     @allure.step("Clicking the news link in the header")
     def click_new_link(self) -> "EcoNewsPage":
         """Click the news link in the header and return an instance of the EcoNewsPage."""
-        from pages.eco_news_page import \
-            EcoNewsPage  # pylint: disable=import-outside-toplevel
+        from pages.eco_news_page import EcoNewsPage  # pylint: disable=import-outside-toplevel
 
         logger.info("Clicking the news link in the header.")
 
         try:
-            WebDriverWait(self.root.parent, 10).until(
-                EC.element_to_be_clickable(self.new_link_locator)
-            ).click()
+            WebDriverWait(self.root.parent,
+                          10).until(EC.element_to_be_clickable(self.new_link_locator)).click()
             logger.info("Successfully clicked the news link in the header.")
             return EcoNewsPage(self.root.parent)
 
@@ -45,17 +41,14 @@ class HeaderComponent(BaseComponent):
     @allure.step("Clicking the event link in the header")
     def click_event_link(self) -> "EventPage":
         """Click the event link in the header and return an instance of the EventPage."""
-        from pages.event_page import \
-            EventPage  # pylint: disable=import-outside-toplevel
-        WebDriverWait(self.root.parent, 10).until(
-            EC.element_to_be_clickable(self.event_link_locator)
-        ).click()
+        from pages.event_page import EventPage  # pylint: disable=import-outside-toplevel
+        WebDriverWait(self.root.parent,
+                      10).until(EC.element_to_be_clickable(self.event_link_locator)).click()
         return EventPage(self.root.parent)
 
     @allure.step("Clicking signin button in the header")
     def click_sign_in_link(self) -> SignInComponent:
         """Click the sign in link in the header and return an instance of the SignInComponent."""
-        WebDriverWait(self.root.parent, 10).until(
-            EC.element_to_be_clickable(self.sign_in_link_locator)
-        ).click()
+        WebDriverWait(self.root.parent,
+                      10).until(EC.element_to_be_clickable(self.sign_in_link_locator)).click()
         return SignInComponent(self.root.parent)
--- pages/eco_news_page.py
+++ pages/eco_news_page.py
@@ -1,21 +1,29 @@
 """This module contains the EcoNewsPage class, which represents the Eco News page of the website."""
+from typing import Tuple
 
+import allure
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
 
+from components.button_create_new_component import CreateNewButtonComponent
 from components.news_components.news_card_base_component import \
     NewsCardBaseComponent
 from pages.base_page import BasePage
-from utils.types import Locators
+from pages.create_update_eco_news_page import CreateUpdateEcoNewsPage
+from pages.event_page import EventPage
 
 
 class EcoNewsPage(BasePage):
     """Page object for the Eco News page."""
+    Locators = Tuple[str, str]
+
     main_header_locator: Locators = (By.CSS_SELECTOR, ".cont >.main-header")
+    button_create_news_locator: Locators = (By.XPATH, "//*[@id='main-content']/div/div[1]/div/a")
     news_cards_locator: Locators = (By.CSS_SELECTOR,
                                     ".ng-star-inserted .gallery-view-li-active")
-
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
         self.main_header: WebElement = self.driver.find_element(
@@ -25,3 +33,22 @@ class EcoNewsPage(BasePage):
             for element
             in self.driver.find_elements(*self.news_cards_locator)
         ]
+    def get_new_button_component(self):
+        """Get the create news button component."""
+        return CreateNewButtonComponent(self.find(self.button_create_news_locator))
+
+    @allure.step("Clicking the create news button")
+    def click_create_button(self):
+        """Click the create news link in the page and return an instance of the
+        CreateUpdateEcoNews."""
+        self.get_new_button_component().click_create_new_button()
+        return CreateUpdateEcoNewsPage(self.driver)
+
+    @allure.step("Navigating to the Events page")
+    def go_to_events(self) -> "EventPage":
+        """Navigate to the Eco News page."""
+        self.header.click_event_link()
+        WebDriverWait(self.driver, 10).until(
+            EC.url_contains("events")
+        )
+        return EventPage(self.driver)
