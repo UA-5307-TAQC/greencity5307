b531b0c3801f8e3b3c9316df52f78abbba04eb7b
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-02-21 12:37:40 +0200

    "Creating of test with two buttons check"

--- components/header_component.py
+++ components/header_component.py
@@ -20,18 +20,18 @@ class HeaderComponent(BaseComponent):
     event_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/events']")
     sign_in_link_locator: Locators = (By.CSS_SELECTOR,
                                       ".header_navigation-menu-right-list > .header_sign-in-link")
+    about_us_link_locator: Locators = (By.XPATH, ".//a[@href='#/greenCity/about']")
 
     @allure.step("Clicking the news link in the header")
     def click_new_link(self) -> "EcoNewsPage":
         """Click the news link in the header and return an instance of the EcoNewsPage."""
-        from pages.eco_news_page import EcoNewsPage  # pylint: disable=import-outside-toplevel
-
         logger.info("Clicking the news link in the header.")
 
         try:
             WebDriverWait(self.root.parent,
                           10).until(EC.element_to_be_clickable(self.new_link_locator)).click()
             logger.info("Successfully clicked the news link in the header.")
+            from pages.eco_news_page import EcoNewsPage # pylint: disable=import-outside-toplevel
             return EcoNewsPage(self.root.parent)
 
         except Exception:
@@ -41,14 +41,21 @@ class HeaderComponent(BaseComponent):
     @allure.step("Clicking the event link in the header")
     def click_event_link(self) -> "EventPage":
         """Click the event link in the header and return an instance of the EventPage."""
-        from pages.event_page import EventPage  # pylint: disable=import-outside-toplevel
         WebDriverWait(self.root.parent,
                       10).until(EC.element_to_be_clickable(self.event_link_locator)).click()
+        from pages.event_page import EventPage # pylint: disable=import-outside-toplevel
         return EventPage(self.root.parent)
 
     @allure.step("Clicking signin button in the header")
     def click_sign_in_link(self) -> SignInComponent:
-        """Click the sign in link in the header and return an instance of the SignInComponent."""
+        """Click the sign-in link in the header and return an instance of the SignInComponent."""
         WebDriverWait(self.root.parent,
                       10).until(EC.element_to_be_clickable(self.sign_in_link_locator)).click()
         return SignInComponent(self.root.parent)
+
+    @allure.step("Clicking the About Us link in the header")
+    def click_about_us_link(self):
+        """Click the About Us link in the header and return an instance of the AboutUsPage."""
+        WebDriverWait(self.root.parent, 10).until(
+            EC.element_to_be_clickable(self.about_us_link_locator)
+        ).click()
--- pages/about_us_page.py
+++ pages/about_us_page.py
@@ -1,7 +1,10 @@
 """This module contains the AboutUsPage class,which represents the about_us page of a website."""
 from selenium.webdriver.common.by import By
 
+from components.about_us_buttons_component import AboutUsPageHabitButtonOneComponent, \
+    AboutUsPageHabitButtonTwoComponent
 from pages.base_page import BasePage
+from pages.my_habit_page import MyHabitPage
 from utils.types import Locators
 
 
@@ -10,8 +13,8 @@ class AboutUsPage(BasePage):
 
     section_header_one: Locators = (By.XPATH, "//*[@id='main-content']/div[1]/div/h2")
     section_description_one: Locators = (By.XPATH, "//*[@id='main-content']/div[1]/div/p")
-    section_button_form_habit_one: Locators = (By.XPATH,
-                                               "//*[@id='main-content']/div[1]/div/button")
+    section_button_form_habit_one: Locators = (By.CSS_SELECTOR,
+        "#main-content > div.about-section.section > div > button")
 
     section_header_two: Locators = (By.XPATH, "//*[@id='main-content']/div[2]/div/div/h2")
     section_description_two: Locators = (By.XPATH, "//*[@id='main-content']/div[2]/div/div/p")
@@ -22,9 +25,22 @@ class AboutUsPage(BasePage):
 
     vision_cards: Locators = (By.CSS_SELECTOR, ".container > .vision-card")
 
+    def get_button_one_component(self):
+        """Get the locator for the create news button."""
+        return AboutUsPageHabitButtonOneComponent(self.find(self.section_button_form_habit_one))
+    def get_button_two_component(self):
+        """Get the locator for the create news button."""
+        return AboutUsPageHabitButtonTwoComponent(self.find(self.section_button_form_habit_two))
+
     def click_section_button_form_habit_one(self):
-        """Clicks the section button form habit."""
-        self.click(self.section_button_form_habit_one)
+        """Click the form habit button one and return my habits page."""
+        self.get_button_one_component().click_form_habit_button_one()
+        return MyHabitPage(self.driver)
+
+    def click_section_button_form_habit_two(self):
+        """Click the form habit button two and return my habits page."""
+        self.get_button_two_component().click_form_habit_button_two()
+        return MyHabitPage(self.driver)
 
     def get_vision_cards_count(self):
         """Gets the number of vision cards present in the section."""
