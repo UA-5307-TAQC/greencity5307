003960af43faf0ef26255afe43fa227931c16be6
Valentayne <yehoianvb0510@gmail.com>
2026-02-27 00:01:20 +0200

    test case logic update

--- tests/ui/my_habit_tests/test_slider_boundaries.py
+++ tests/ui/my_habit_tests/test_slider_boundaries.py
@@ -23,14 +23,16 @@ def test_slider_keyboard_interaction(driver: WebDriver):
 
     one_habit_page = OneHabitPage(driver)
 
+    sldier_value = one_habit_page.get_slider_value()
     one_habit_page.move_slider_right(3)
-    assert one_habit_page.get_slider_value() == 10, "Slider should display 10"
+    assert one_habit_page.get_slider_value() == sldier_value + 3, f"Slider should display {sldier_value + 3}"
 
+    sldier_value = one_habit_page.get_slider_value()
     one_habit_page.move_slider_left(1)
-    assert one_habit_page.get_slider_value() == 9, "Slider should display 9"
+    assert one_habit_page.get_slider_value() == sldier_value - 1, f"Slider should display {sldier_value - 1}"
 
-    one_habit_page.move_slider_left(1)
+    one_habit_page.move_slider_left(57)
     assert one_habit_page.get_slider_value() == 7, "Slider should not go below 7"
 
-    one_habit_page.move_slider_right(50)
+    one_habit_page.move_slider_right(57)
     assert one_habit_page.get_slider_value() == 56, "Slider should not exceed 56"
