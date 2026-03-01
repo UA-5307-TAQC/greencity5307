023f7ac2f7eeed2ae396182b7e53d98bcdefb395
Kosya15 <hrumak15@gmail.com>
2026-03-01 12:33:14 +0200

    refactor: updated pages and components to PF

--- components/events_components/event_card_component.py
+++ components/events_components/event_card_component.py
@@ -1,123 +1,149 @@
 """Component class for an event card element on a web page."""
+from typing import List
+
 from selenium.webdriver.common.by import By
 
 from components.base_component import BaseComponent
+
+from utils.custom_web_element import CustomWebElement
 
 
 class EventCardComponent(BaseComponent):
     """Component class for an event card element on a web page."""
 
-    save_flag_locator = (By.CSS_SELECTOR, ".event-flags")
-    event_tags_locator = (By.CSS_SELECTOR, ".ul-eco-buttons .text")
+    locators = {
+        "save_flag": (By.CSS_SELECTOR, ".event-flags"),
+        "event_tags": (By.CSS_SELECTOR, ".ul-eco-buttons .text"),
 
-    event_date_locator = (By.CSS_SELECTOR, ".date")
-    event_time_locator = (By.CSS_SELECTOR, ".time")
-    event_place_locator = (By.CSS_SELECTOR, "span.place + p")
-    event_status_locator = (By.CSS_SELECTOR, ".event-status")
+        "event_date": (By.CSS_SELECTOR, ".date"),
+        "event_time": (By.CSS_SELECTOR, ".time"),
+        "event_place": (By.CSS_SELECTOR, "span.place + p"),
+        "event_status": (By.CSS_SELECTOR, ".event-status"),
 
-    event_title_locator = (By.CSS_SELECTOR, ".event-name")
+        "event_title": (By.CSS_SELECTOR, ".event-name"),
 
-    more_button_locator = (By.CSS_SELECTOR, ".secondary-global-button")
-    join_button_locator = (By.CSS_SELECTOR, ".primary-global-button")
+        "more_button": (By.CSS_SELECTOR, ".secondary-global-button"),
+        "join_button": (By.CSS_SELECTOR, ".primary-global-button"),
 
-    created_date_locator = (By.CSS_SELECTOR, ".additional-info .date p")
-    author_locator = (By.CSS_SELECTOR, ".additional-info .author p")
+        "event_created_date": (By.CSS_SELECTOR, ".additional-info .date p"),
+        "event_author": (By.CSS_SELECTOR, ".additional-info .author p"),
 
-    comments_count_locator = (By.CSS_SELECTOR, ".additional-info .frame p")
+        "event_comments_count": (By.CSS_SELECTOR, ".additional-info .frame p"),
 
-    like_button_locator = (By.CSS_SELECTOR, ".additional-info .like")
-    like_count_locator = (By.CSS_SELECTOR, ".additional-info .like span")
+        "like_button": (By.CSS_SELECTOR, ".additional-info .like"),
+        "event_like_count": (By.CSS_SELECTOR, ".additional-info .like span"),
 
-    dislike_button_locator = (By.CSS_SELECTOR, ".additional-info .dislike")
-    dislike_count_locator = (By.CSS_SELECTOR, ".additional-info .dislike span")
+        "dislike_button": (By.CSS_SELECTOR, ".additional-info .dislike"),
+        "event_dislike_count": (By.CSS_SELECTOR, ".additional-info .dislike span")
+    }
 
-    def more(self):
+    save_flag: CustomWebElement
+    event_tags: List[CustomWebElement]
+
+    event_date: CustomWebElement
+    event_time: CustomWebElement
+    event_place: CustomWebElement
+    event_status: CustomWebElement
+
+    event_title: CustomWebElement
+
+    more_button: CustomWebElement
+    join_button: CustomWebElement
+
+    event_created_date: CustomWebElement
+    event_author: CustomWebElement
+
+    event_comments_count: CustomWebElement
+
+    like_button: CustomWebElement
+    event_like_count: CustomWebElement
+
+    dislike_button: CustomWebElement
+    event_dislike_count: CustomWebElement
+
+    def click_more(self):
         """Click the 'More' button on the event card."""
-        self.root.find_element(*self.more_button_locator).click()
+        self.more_button.wait_and_click()
         from pages.events_pages.one_event_page import OneEventPage  # pylint: disable=import-outside-toplevel
         return OneEventPage(self.root.parent)
 
-    def join_event(self):
+    def click_join_event(self):
         """Click the 'Join' button on the event card."""
-        join_button_element = self.root.find_element(*self.join_button_locator)
-        if "Cancel" not in join_button_element.text:
-            join_button_element.click()
+        if "Cancel" not in self.join_button.text:
+            self.join_button.wait_and_click()
 
     def cancel_request(self):
         """Click the button to cancel the join request if it's currently joined."""
-        join_button_element = self.root.find_element(*self.join_button_locator)
-        if "Cancel" in join_button_element.text:
-            join_button_element.click()
+        if "Cancel" in self.join_button.text:
+            self.join_button.wait_and_click()
 
-    def save_event(self):
+    def click_save_event(self):
         """Click the button to save the event if it's not already saved."""
-        self.root.find_element(*self.save_flag_locator).click()
+        self.save_flag.wait_and_click()
 
     def is_saved(self) -> bool:
         """Check if the event is saved."""
-        save_flag_element = self.root.find_element(*self.save_flag_locator)
-        return "flag-active" in save_flag_element.get_attribute("class")
+        return "flag-active" in self.save_flag.get_attribute("class")
 
     @property
     def tags(self):
         """Return a list of tags associated with the event."""
-        tag_elements = self.root.find_elements(*self.event_tags_locator)
-        return [tag.text for tag in tag_elements]
+        return [tag.text for tag in self.event_tags]
 
     @property
     def date(self):
         """Return the date of the event."""
-        return self.root.find_element(*self.event_date_locator).text
+        return self.event_date.text
 
     @property
     def time(self):
         """Return the time of the event."""
-        return self.root.find_element(*self.event_time_locator).text
+        return self.event_time.text
 
     @property
     def place(self):
         """Return the place of the event."""
-        return self.root.find_element(*self.event_place_locator).text
+        return self.event_place.text
 
     @property
     def status(self):
         """Return the status of the event."""
-        return self.root.find_element(*self.event_status_locator).text
+        return self.event_status.text
 
     @property
     def title(self):
         """Return the title of the event."""
-        return self.root.find_element(*self.event_title_locator).text
+        return self.event_title.text
 
     @property
     def created_date(self):
         """Return the created date of the event."""
-        return self.root.find_element(*self.created_date_locator).text
+        return self.event_created_date.text
 
     @property
     def author(self):
         """Return the author of the event."""
-        return self.root.find_element(*self.author_locator).text
+        return self.event_author.text
 
     @property
     def comments_count(self):
         """Return the number of comments on the event."""
-        return int(self.root.find_element(*self.comments_count_locator).text)
+        return int(self.event_comments_count.text)
 
     @property
     def like_count(self):
         """Return the number of likes on the event."""
-        return int(self.root.find_element(*self.like_count_locator).text)
+        return int(self.event_like_count.text)
 
     @property
     def dislike_count(self):
         """Return the number of dislikes on the event."""
-        return int(self.root.find_element(*self.dislike_count_locator).text)
+        return int(self.event_dislike_count.text)
 
-    def like(self):
+    def click_like(self):
         """Click the 'Like' button on the event card."""
-        self.root.find_element(*self.like_button_locator).click()
+        self.like_button.wait_and_click()
 
-    def dislike(self):
+    def click_dislike(self):
         """Click the 'Dislike' button on the event card."""
-        self.root.find_element(*self.dislike_button_locator).click()
+        self.dislike_button.wait_and_click()
--- pages/abstract_pages/friends_abstract/find_friend_page.py
+++ pages/abstract_pages/friends_abstract/find_friend_page.py
@@ -1,5 +1,7 @@
 """This module contains the page object for the find_friend page."""
 from selenium.webdriver.common.by import By
+import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 from pages.abstract_pages.friends_abstract.friends_abstract_page \
     import FriendsAbstractPage
@@ -11,6 +13,10 @@ from components.abstract_pages_components.friends_components \
 class FindFriendPage(FriendsAbstractPage):
     """Page object class for the Find Friend page."""
 
+    locators = {
+        "friend_card": (By.CSS_SELECTOR, ".user-card", FriendCardComponent)
+    }
+
     _friend_card_by_name_pattern = ("//div[contains(@class, 'user-card')]"
                                     "[.//p[contains(@class, 'friend-name') "
                                     "and contains(text(), '{}')]]")
@@ -20,3 +26,12 @@ class FindFriendPage(FriendsAbstractPage):
         formatted_pattern = self._friend_card_by_name_pattern.format(name)
         friend_card_element = self.driver.find_element(By.XPATH, formatted_pattern)
         return FriendCardComponent(friend_card_element)
+
+    def is_page_loaded(self) -> bool:
+        try:
+            self.get_wait().until(
+                EC.visibility_of_element_located(self.locators["friend_card"])
+            )
+            return True
+        except TimeoutException:
+            return False
