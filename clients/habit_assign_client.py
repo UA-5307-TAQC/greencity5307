c12679cfa9f850f8a2e9559e3e0d0d5f48296105
Liubov Titova <titova.l7789@gmail.com>
2026-03-04 18:30:44 +0200

    api test for habit assignment and deletion

---
+++ clients/habit_assign_client.py
@@ -0,0 +1,47 @@
+"""Client for interacting with the Habit API, extending BaseClient for common functionality."""
+
+from requests import Response
+
+from data.config import Config
+from clients.base_client import BaseClient
+
+
+class HabitAssignClient(BaseClient):
+    """Client for interacting with the Habit API,
+     extending BaseClient for common functionality."""
+
+
+    def __init__(self, base_url, access_token=None):
+        super().__init__(base_url=f"{base_url}/habit/assign", access_token=access_token)
+
+
+    def assign_habit_with_default_properties(self, habit_id: int) -> Response:
+        """Assign habit with default properties"""
+
+        return self._request(
+            method="POST",
+            endpoint=f"/{habit_id}"
+        )
+
+
+    def get_all_assigned_habits(self, lang: str = Config.BROWSER_LANG) -> Response:
+        """Find all assigned habits"""
+
+        params = {
+            "lang": lang,
+        }
+
+        return self._request(
+            method="GET",
+            endpoint="/allForCurrentUser",
+            params=params
+        )
+
+
+    def delete_habit_assign(self, habit_assign_id: int) -> Response:
+        """Delete assigned habit"""
+
+        return self._request(
+            method="DELETE",
+            endpoint=f"/delete/{habit_assign_id}"
+        )
