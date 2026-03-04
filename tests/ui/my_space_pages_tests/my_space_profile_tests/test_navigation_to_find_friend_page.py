161efae68eb55bcd3a91a9530bfb99e14ca42ee7
Liubov Titova <titova.l7789@gmail.com>
2026-03-04 13:06:20 +0200

    test navigation to find friend page

---
+++ tests/ui/my_space_pages_tests/my_space_profile_tests/test_navigation_to_find_friend_page.py
@@ -0,0 +1,20 @@
+import allure
+
+from pages.abstract_pages.my_space_abstract.my_habit_page import MyHabitPage
+
+@allure.title("Check that user is redirected to Edit Profile page "
+"after clicking on the [Edit] button")
+def test_navigation_to_edit_profile_page(driver_with_login):
+    """Test correct navigation to Edit Profile page."""
+
+    with allure.step("Go to My Space page"):
+        driver = driver_with_login
+        my_habit_page = MyHabitPage(driver)
+
+    with allure.step("Click [Edit] button"):
+        profile_banner = my_habit_page.profile_banner
+        profile_banner.click_edit_btn()
+
+    with allure.step("Check if user is redirected to Edit Profile page"):
+        url = driver.current_url
+        assert "edit" in url, f"Expected to be on friends page, but was at: {url}"
