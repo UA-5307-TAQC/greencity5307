8caf54158807cdbad21a3ef6f3f4661e0d0181bc
D1xiS <oktaviosilva96@gmail.com>
2026-03-05 18:07:58 +0200

    feat: add mutual friends API test with dynamic fixture and JSON schema validation.

---
+++ clients/friends_client.py
@@ -0,0 +1,33 @@
+"""Client for interacting with Friends API."""
+
+import allure
+from requests import Response
+from clients.base_client import BaseClient
+
+class FriendsClient(BaseClient):
+    """Client for interacting with Friends API."""
+
+    def __init__(self, base_url, access_token=None):
+        """Init FriendsClient."""
+        super().__init__(base_url=f"{base_url}/friends", access_token=access_token)
+
+    @allure.step("Get list of all friends")
+    def get_all_friends(self, page: int = 0, size: int = 10) -> Response:
+        """Get all friends list."""
+
+        params = {
+            "page": page,
+            "size": size
+        }
+
+        return self._request("Get", "", params=params)
+
+    @allure.step("Get mutual friends.")
+    def get_mutual_friends(self, friend_id: int, page: int = 0, size: int = 10) -> Response:
+        """Fetch mutual friends."""
+        params = {
+            "friendId": friend_id,
+            "page": page,
+            "size": size
+        }
+        return self._request("Get", "/mutual-friends", params=params)
