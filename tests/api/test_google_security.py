91fc1781779d424396e1bc54eeeed11aa37e97e1
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-11 13:21:48 +0200

    creation of Google security schema for api test

--- tests/api/test_google_security.py
+++ tests/api/test_google_security.py
@@ -1,8 +1,14 @@
 """Test for validation GET request of Google security"""
+from json import JSONDecodeError
+
+import allure
 import pytest
+from jsonschema.validators import validate
 
 from clients.google_security_client import GoogleSecurityClient
 from data.config import Config
+from schemas.google_security_schema import google_security_schema
+from utils.logger import logger
 
 
 @pytest.mark.parametrize(
@@ -18,4 +24,33 @@ def test_google_security(project_name: str, lang: str, access_token):
 
     response = client.get_google_security(project_name, lang)
 
-    assert response.status_code == 200, response.text
+    if response.status_code == 200:
+        with allure.step("Validate proper response json format"):
+            parsed_data = response.json()
+            logger.info(parsed_data)
+            try:
+                validate(instance=parsed_data, schema=google_security_schema)
+                logger.info("✅ JSON validation passed")
+            except JSONDecodeError as e:
+                allure.attach(str(e), name="Validation Error",
+                              attachment_type=allure.attachment_type.TEXT)
+                pytest.fail(f"Response JSON does not match schema: {e}")
+    elif response.status_code == 400:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+
+        assert parsed_data["message"] == "Current user has no permission for this action"
+    elif response.status_code == 404:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["message"] == "Page is not found"
+    elif response.status_code == 403:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["message"] == "Unforbidden access"
+    elif response.status_code == 500:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data.get("error") == "Internal Server Error"
+    else:
+        assert False, "Other error"
