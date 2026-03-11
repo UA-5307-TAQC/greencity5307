eefad489395d3be89d4e6a05f025d94f6408c140
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-11 14:48:07 +0200

    creation of social networks schema for api test

---
+++ schemas/social_networks_schema.py
@@ -0,0 +1,35 @@
+"""Create Eco New Schema"""
+
+social_networks_image_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "required": [
+        "id",
+        "imagePath",
+        "hostPath",
+    ],
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "imagePath": {
+            "type":"string"
+        },
+        "hostPath": {
+            "type":"string"
+        }
+    }
+}
+
+social_networks_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "required": [
+        "id"
+    ],
+    "properties": {
+        "id": {
+            "type": "integer"
+        }
+    }
+}
