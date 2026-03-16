d38c4666a3301867fb3f823ec41c854ee6db64d9
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-16 17:15:28 +0200

    create eco news get summary schema

---
+++ schemas/news/summary_eco_new_schema.py
@@ -0,0 +1,16 @@
+"""Summary eco new schema"""
+summary_eco_new_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "required": [
+        "content"
+    ],
+    "properties": {
+        "content": {
+            "type": "string"
+        },
+        "source": {
+            "type": ["string" ,"null"]
+        }
+    }
+}
