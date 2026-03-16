35c11ca0913f0bbc17a960fb37a3e09bedd1b700
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-16 16:34:25 +0200

    create of social network delete test

---
+++ tests/api/social_networks/test_delete_social_network.py
@@ -0,0 +1,64 @@
+"""Social network delete by id verification"""
+# pylint: disable=duplicate-code
+import allure
+import pytest
+from jsonschema.exceptions import ValidationError
+from jsonschema.validators import validate
+
+from clients.social_network_client import SocialNetworkClient
+from data.config import Config
+from schemas.social_networks_schema import social_networks_schema
+from utils.logger import logger
+
+
+@pytest.mark.parametrize(
+    "network_id " ,[
+        "1",
+        "2",
+        "3",
+        "4"
+    ]
+)
+@allure.feature("SocialNetwork")
+@allure.title("Check user access to the deletion of social media.")
+def test_social_networks_delete_by_id(network_id, access_token):
+    """Test for deletion of social_network by id verification"""
+    token = access_token
+
+    with allure.step("Create social networks client"):
+        client = SocialNetworkClient(
+            base_url=Config.BASE_USER_API_URL,
+            access_token=token
+        )
+
+    response = client.delete_social_network_by_id(network_id=network_id)
+
+    if response.status_code == 200:
+        with allure.step("Validate proper response json format"):
+            parsed_data = response.json()
+            logger.info(parsed_data)
+            try:
+                validate(instance=parsed_data, schema=social_networks_schema)
+                logger.info("JSON validation passed")
+            except ValidationError as e:
+                allure.attach(str(e), name="Validation Error",
+                              attachment_type=allure.attachment_type.TEXT)
+                pytest.fail(f"Response JSON does not match schema: {e}")
+    elif response.status_code == 400:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["message"] == "Current user has no permission for this action"
+    elif response.status_code == 404:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["message"] == "Page is not found"
+    elif response.status_code == 403:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["error"] == "Forbidden"
+    elif response.status_code == 500:
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data.get("error") == "Internal Server Error"
+    else:
+        assert False, "Other error"
