27ba6ab3cab99669ac96339efeea6c2d931913b4
Liubov Titova <titova.l7789@gmail.com>
2026-03-16 16:40:05 +0200

    fix

--- pages/abstract_pages/friends_abstract/find_friend_page.py
+++ pages/abstract_pages/friends_abstract/find_friend_page.py
@@ -49,7 +49,6 @@ class FindFriendPage(FriendsAbstractPage):
         except NoSuchElementException:
             return None
 
-
     def wait_for_list_to_load(self) -> None:
         """Waits until at least one friend card is visible on the page."""
         self.get_wait().until(EC.visibility_of_element_located(self.locators["cards"][:2]))
