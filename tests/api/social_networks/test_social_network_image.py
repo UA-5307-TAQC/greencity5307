824af4ca7bcfaa5c91f164519617c70980a0f677
Liubomyr Halamaha <galamagal86@gmail.com>
2026-03-17 13:39:42 +0200

    update

--- tests/api/social_networks/test_social_network_image.py
+++ tests/api/social_networks/test_social_network_image.py
@@ -7,21 +7,22 @@ from jsonschema import validate
 
 from clients.social_network_client import SocialNetworkClient
 from data.config import Config
-from schemas.social_networks_schema import social_networks_schema
+from schemas.social_networks_schema import social_networks_image_schema
 from utils.logger import logger
 
 
 @pytest.mark.parametrize(
-    "image_url " ,[
-        "https://picsum.photos/200",
-        "https://picsum.photos/200",
-        "https://picsum.photos/200",
-        "https://picsum.photos/200"
+    "image_url, status_code, message_type, message" ,[
+        ("https://picsum.photos/200",200, "", ""),
+        ("https://picsum.photos/400",400, "message", "Current user has no permission for this action"),
+        ("https://picsum.photos/404",404, "message", "Page is not found"),
+        ("https://picsum.photos/403",403, "error", "Forbidden"),
+        ("https://picsum.photos/500",500, "error", "Internal Server Error")
     ]
 )
 @allure.feature("SocialNetwork")
 @allure.title("Check user access for image of social network.")
-def test_social_networks_get_image(image_url,  access_token):
+def test_social_networks_get_image(image_url, status_code, message_type, message, access_token):
     """Testing of social networks image get request"""
     token = access_token
 
@@ -37,27 +38,14 @@ def test_social_networks_get_image(image_url,  access_token):
             parsed_data = response.json()
             logger.info(parsed_data)
             try:
-                validate(instance=parsed_data, schema=social_networks_schema)
+                validate(instance=parsed_data, schema=social_networks_image_schema)
                 logger.info("JSON validation passed")
             except ValidationError as e:
                 allure.attach(str(e), name="Validation Error",
                               attachment_type=allure.attachment_type.TEXT)
                 pytest.fail(f"Response JSON does not match schema: {e}")
-    elif response.status_code == 400:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == "Current user has no permission for this action"
-    elif response.status_code == 404:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == "Page is not found"
-    elif response.status_code == 403:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["error"] == "Forbidden"
-    elif response.status_code == 500:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data.get("error") == "Internal Server Error"
     else:
-        assert False, "Other error"
+        assert response.status_code == status_code, f"Expected status code {status_code}, got {response.status_code}"
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data[message_type] == message, f"Expected message '{message}', got '{parsed_data[message_type]}'"
