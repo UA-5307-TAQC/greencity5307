51826f25b290d07ec08b3465af130a9dbbb3d3b4
Valentayne <yehoianvb0510@gmail.com>
2026-03-17 13:13:49 +0200

    add schema validation

--- tests/api/own-security-controller_tests/test_login_validation.py
+++ tests/api/own-security-controller_tests/test_login_validation.py
@@ -3,8 +3,13 @@ import pytest
 import jwt
 import allure
 from datetime import datetime
+
+from jsonschema import validate, ValidationError
+
 from clients.own_security_client import OwnSecurityClient
 from data.config import Config
+from schemas.own_security.signin_success_schema import signin_success_schema
+
 
 @allure.feature("Authentication")
 @allure.story("User Login")
@@ -24,15 +29,20 @@ def test_login_validation():
         response_time_ms = response.elapsed.total_seconds() * 1000
         assert response_time_ms < 3000, f"API response is too slow! Time: {response_time_ms:.2f} ms"
 
+    data = response.json()
+    with allure.step("Validate response JSON schema"):
+        try:
+            validate(instance=data, schema=signin_success_schema)
+        except ValidationError as e:
+            pytest.fail(f"Response JSON schema validation failed: {e.message}")
+
     with allure.step("Validate response body data"):
-        data = response.json()
-        assert data.get("userId") is not None and data.get("userId") >= 0, "User ID is invalid or missing"
+        assert data.get("userId") >= 0, "User ID should be a positive integer"
         assert data.get("name") == Config.USER_NAME, f"Expected {Config.USER_NAME}, but got '{data.get('name')}'"
         assert data.get("ownRegistrations") is True, "ownRegistrations flag should be True"
 
     with allure.step("Validate Access Token expiration"):
         token = data.get("accessToken")
-        assert token is not None, "Access token is missing in the response body"
 
         try:
             decoded = jwt.decode(token, options={"verify_signature": False})
@@ -41,7 +51,6 @@ def test_login_validation():
             allure.attach(str(decoded), name="Decoded Token Payload", attachment_type=allure.attachment_type.TEXT)
 
             assert exp_timestamp is not None, "Token is valid, but missing expiration time ('exp' field)"
-
             assert datetime.now().timestamp() < exp_timestamp, f"Access token is already expired! (exp: {exp_timestamp})"
         except Exception as e:
             pytest.fail(f"Token is invalid or corrupted. Details: {e}")
