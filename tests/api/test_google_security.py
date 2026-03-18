07f1b087987d3da8dc361d85104fbd09d435a711
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-18 17:30:26 +0200

    refactoring

--- tests/api/test_google_security.py
+++ tests/api/test_google_security.py
@@ -1,9 +1,9 @@
 """Test for validation GET request of Google security"""
-from json import JSONDecodeError
 
 import allure
 import pytest
-from jsonschema.validators import validate
+from jsonschema.exceptions import ValidationError
+from jsonschema import validate
 
 from clients.google_security_client import GoogleSecurityClient
 from data.config import Config
@@ -12,45 +12,38 @@ from utils.logger import logger
 
 
 @pytest.mark.parametrize(
-    "project_name, lang",
-    [("GREENCITY" , "en"),
-     ("GREENCITY", "uk")
+    "project_name, lang, status_code, message_type,  message",
+    [("GREENCITY" , "en", 200, "", ""),
+     ("GREENCITY", "uk", 200,"", ""),
+     ("", "uk", 404, "message" ,"Page is not found")
      ]
 )
-def test_google_security(project_name: str, lang: str, access_token):
+# pylint: disable=too-many-positional-arguments
+def test_google_security(project_name: str, lang: str,
+                         status_code, message_type, message, access_token):
     """Test for validation of Google security authentication."""
 
     client = GoogleSecurityClient(base_url=Config.BASE_USER_API_URL, access_token=access_token)
 
     response = client.get_google_security(project_name, lang)
 
-    if response.status_code == 200:
+    if response.status_code == status_code:
         with allure.step("Validate proper response json format"):
             parsed_data = response.json()
             logger.info(parsed_data)
             try:
                 validate(instance=parsed_data, schema=google_security_schema)
-                logger.info("✅ JSON validation passed")
-            except JSONDecodeError as e:
+                logger.info("JSON validation passed")
+            except ValidationError as e:
                 allure.attach(str(e), name="Validation Error",
-                              attachment_type=allure.attachment_type.TEXT)
+                      attachment_type=allure.attachment_type.TEXT)
                 pytest.fail(f"Response JSON does not match schema: {e}")
-    elif response.status_code == 400:
-        parsed_data = response.json()
-        logger.info(parsed_data)
+            else:
+                pytest.fail(f"Response JSON does not match schema: {status_code}")
 
-        assert parsed_data["message"] == "Current user has no permission for this action"
-    elif response.status_code == 404:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == "Page is not found"
-    elif response.status_code == 403:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == "Unforbidden access"
-    elif response.status_code == 500:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data.get("error") == "Internal Server Error"
-    else:
-        assert False, "Other error"
+    assert response.status_code == status_code, \
+        f"Expected status code {status_code}, got {response.status_code}"
+    parsed_data = response.json()
+    logger.info(parsed_data)
+    assert parsed_data[message_type] == message, \
+        f"Expected message '{message}', got '{parsed_data[message_type]}'"
