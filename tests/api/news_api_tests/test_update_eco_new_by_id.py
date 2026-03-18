2c7cfd88075b39c26b129bcb7c1c48ea5c6b0c2f
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-18 18:35:05 +0200

    refactoring

--- tests/api/news_api_tests/test_update_eco_new_by_id.py
+++ tests/api/news_api_tests/test_update_eco_new_by_id.py
@@ -23,7 +23,7 @@ def image_file():
             return tmp.name
 
 @pytest.mark.parametrize(
-    "news_id, data",
+    "news_id, data, status_code",
     [
         ("32",{
             "id" : "32",
@@ -40,7 +40,7 @@ def image_file():
                 "content": "Full article text here",
                 "languageCode": "en"
             }
-        }),
+        }, 201),
         ("45", {
             "id" : "45",
             "source": "https://example.com",
@@ -56,7 +56,7 @@ def image_file():
                 "content":"Details about the new product",
                 "languageCode":"en"
             }
-        }),
+        }, 201),
         ("78", {
             "id" : "78",
             "source": "https://example.net",
@@ -72,14 +72,14 @@ def image_file():
                 "content":"Event details and registration info",
                 "languageCode":"en"
             }
-        })
+        }, 201)
     ],
 )
 
 @allure.feature("UpdateEcoNew")
 @allure.story("Update new")
 @allure.title("Update eco new by applying valid parameters to the form.")
-def test_update_eco_news_put_request_with_valid_data(news_id, data: dict, access_token):
+def test_update_eco_news_put_request_with_valid_data(news_id, data: dict,status_code, access_token):
     """Test create eco news request with valid data"""
     token = access_token
 
@@ -101,19 +101,77 @@ def test_update_eco_news_put_request_with_valid_data(news_id, data: dict, access
             logger.info(parsed_data)
             try:
                 validate(instance=parsed_data, schema=one_news_get_by_id_schema)
-                logger.info("✅ JSON validation passed")
+                logger.info("JSON validation passed")
             except ValidationError as e:
                 allure.attach(str(e), name="Validation Error",
-                              attachment_type=allure.attachment_type.TEXT)
+                      attachment_type=allure.attachment_type.TEXT)
                 pytest.fail(f"Response JSON does not match schema: {e}")
-    elif response.status_code == 400:
-        parsed_data = response.json()
-        logger.info(parsed_data)
+            else:
+                pytest.fail(f"Response JSON does not match schema: {status_code}")
 
-        assert parsed_data["message"] == "Current user has no permission for this action"
-    elif response.status_code == 404:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == f"Eco new doesn\'t exist by this id: {news_id}"
-    else:
-        assert False, "Other error"
+
+@pytest.mark.parametrize(
+    "news_id, data, status_code, message",
+    [
+        ("32",{
+            "id" : "32",
+            "source": "https://example.org",
+            "shortInfo": "Climate change update",
+            "tags": ["not", "not"],
+            "title": "Climate change update",
+            "content": "Full article text here",
+            "titleTranslation": {
+                "content": "Climate News",
+                "languageCode": "not"
+        },
+            "textTranslation": {
+                "content": "Full article text here",
+                "languageCode": "not"
+            }
+        }, 400, "Current user has no permission for this action"),
+        ("78", {
+            "id" : "78",
+            "source": "source",
+            "shortInfo": "Upcoming environmental event",
+            "tags": ["events"],
+            "title": "Upcoming environmental event",
+            "content": "Full article text here",
+            "titleTranslation": {
+                "content":"Environmental Event",
+                "languageCode":"en"
+            },
+            "textTranslation": {
+                "content":"Event details and registration info",
+                "languageCode":"en"
+            }
+        }, 404, "Eco new doesn't exist by this id: 78")
+    ],
+)
+
+@allure.feature("UpdateEcoNew")
+@allure.story("Update new")
+@allure.title("Update eco new by applying invalid parameters to the form.")
+def test_update_eco_news_put_request_with_invalid_data(news_id, data: dict,
+                                                     status_code, message, access_token):
+    """Test create eco news request with invalid data"""
+    token = access_token
+
+    client = EcoNewClient(
+        base_url=Config.BASE_API_URL,
+        access_token=token
+    )
+
+
+    response = client.update_eco_news_by_id(
+        news_id=news_id,
+        image_file_path=image_file(),
+        data=data
+    )
+
+
+    assert response.status_code == status_code, \
+        f"Expected status code {status_code}, got {response.status_code}"
+    parsed_data = response.json()
+    logger.info(parsed_data)
+    assert parsed_data["message"] == message, \
+        f"Expected message '{message}', got '{parsed_data["message"]}'"
