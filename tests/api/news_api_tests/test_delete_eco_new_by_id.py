00965f27c2619253914fab8b1e97d1c0a8d7564d
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-18 17:52:22 +0200

    refactoring

--- tests/api/news_api_tests/test_delete_eco_new_by_id.py
+++ tests/api/news_api_tests/test_delete_eco_new_by_id.py
@@ -1,25 +1,26 @@
 """Test for validation of DELETE request for delete of eco new."""
-from json import JSONDecodeError
-
 import allure
 import pytest
-from jsonschema import validate
+from jsonschema import validate, ValidationError
 
 from clients.eco_new_client import EcoNewClient
 from data.config import Config
 from schemas.news.one_news_schema import one_news_get_by_id_schema
 from utils.logger import logger
 
-
 @pytest.mark.parametrize(
-    "news_id", [
-        "1", "32", "2", "77", "90"
+    "news_id,status_code, message", [
+        ("1", 404, "Eco new doesn't exist by this id: 1"),
+        ("32", 400, "Current user has no permission for this action"),
+        ("2", 404, "Eco new doesn't exist by this id: 2"),
+        ("77", 404, "Eco new doesn't exist by this id: 77"),
+        ("90", 400, "Current user has no permission for this action")
     ]
 )
 @allure.feature("DeleteEcoNew")
 @allure.story("Delete new")
 @allure.title("Delete eco new by id.")
-def test_delete_eco_new_by_id(access_token, news_id: int):
+def test_delete_eco_new_by_id(access_token, news_id: int, status_code, message):
     """Test for validation of correctness of DELETE request."""
     client = EcoNewClient(base_url=Config.BASE_API_URL, access_token=access_token)
     response = client.delete_eco_news_by_id(news_id=news_id)
@@ -29,19 +30,18 @@ def test_delete_eco_new_by_id(access_token, news_id: int):
             logger.info(parsed_data)
             try:
                 validate(instance=parsed_data, schema=one_news_get_by_id_schema)
-                logger.info("✅ JSON validation passed")
-            except JSONDecodeError as e:
+                logger.info("JSON validation passed")
+            except ValidationError as e:
                 allure.attach(str(e), name="Validation Error",
-                              attachment_type=allure.attachment_type.TEXT)
+                      attachment_type=allure.attachment_type.TEXT)
                 pytest.fail(f"Response JSON does not match schema: {e}")
-    elif response.status_code == 400:
-        parsed_data = response.json()
-        logger.info(parsed_data)
+            else:
+                pytest.fail(f"Response JSON does not match schema: {status_code}")
 
-        assert parsed_data["message"] == "Current user has no permission for this action"
-    elif response.status_code == 404:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == f"Eco new doesn\'t exist by this id: {news_id}"
-    else:
-        assert False, "Other error"
+
+    assert response.status_code == status_code, \
+        f"Expected status code {status_code}, got {response.status_code}"
+    parsed_data = response.json()
+    logger.info(parsed_data)
+    assert parsed_data["message"] == message, \
+        f"Expected message '{message}', got '{parsed_data["message"]}'"
