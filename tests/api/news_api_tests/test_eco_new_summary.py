113deb73b1cbbe77bed511fccb1a61252cba8e11
Your NameVitaclnvitalina@macbook-air-vitalina Documents %  git config --global user.name Your <you@example.com>
2026-03-18 19:29:24 +0200

    refactoring

--- tests/api/news_api_tests/test_eco_new_summary.py
+++ tests/api/news_api_tests/test_eco_new_summary.py
@@ -11,15 +11,19 @@ from utils.logger import logger
 
 
 @pytest.mark.parametrize(
-    "news_id", [
-        1, 32, 2, 77, 90
+    "news_id, status_code, message", [
+        ("1", 404, "Eco new doesn't exist by this id: 1"),
+        ("32", 200, ""),
+        ("2", 404, "Eco new doesn't exist by this id: 2"),
+        ( "77", 404, "Eco new doesn't exist by this id: 77"),
+        ("90", 200, "")
     ]
 )
 
 @allure.feature("GetSummaryEcoNew")
 @allure.story("Get summary eco new")
 @allure.title("Get summary eco new by id.")
-def test_eco_new_summary(news_id, access_token):
+def test_eco_new_summary(news_id,status_code, message, access_token):
     """Test eco news summary retrieval by id (GET /eco-news/{id}/summary)"""
     client = EcoNewClient(base_url=Config.BASE_API_URL, access_token=access_token)
     response = client.get_summary_eco_new(news_id=news_id)
@@ -32,23 +36,13 @@ def test_eco_new_summary(news_id, access_token):
                 logger.info("JSON validation passed")
             except ValidationError as e:
                 allure.attach(str(e), name="Validation Error",
-                              attachment_type=allure.attachment_type.TEXT)
+                      attachment_type=allure.attachment_type.TEXT)
                 pytest.fail(f"Response JSON does not match schema: {e}")
-    elif response.status_code == 400:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == "User has already added this eco new to favorites."
-    elif response.status_code == 404:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["message"] == f"Eco new doesn\'t exist by this id: {news_id}"
-    elif response.status_code == 403:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data["error"] == "Forbidden"
-    elif response.status_code == 500:
-        parsed_data = response.json()
-        logger.info(parsed_data)
-        assert parsed_data.get("error") == "Internal Server Error"
+
     else:
-        assert False, "Other error"
+        assert response.status_code == status_code, \
+            f"Expected status code {status_code}, got {response.status_code}"
+        parsed_data = response.json()
+        logger.info(parsed_data)
+        assert parsed_data["message"] == message, \
+            f"Expected message '{message}', got '{parsed_data["message"]}"
