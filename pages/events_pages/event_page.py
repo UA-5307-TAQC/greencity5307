85e360670264aec77357a6bfda7476fc175c3d6a
Osypenko Kostiantyn <133202498+Kosya15@users.noreply.github.com>
2026-03-20 13:50:19 +0200

    added: UI Test Case TC-ES-3 and some small refactors (#192)
    * added: UI Test Case TC-ES-3 and some small refactors
    
    * fix: replaced non-existing method
    
    ---------
    
    Co-authored-by: Liubomyr Halamaha <galamagal86@gmail.com>

--- pages/events_pages/event_page.py
+++ pages/events_pages/event_page.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 from pages.common_pages.places_page import PlacesPage
 from pages.events_pages.create_update_event_page import CreateEventPage
+from components.events_components.event_card_component import EventCardComponent
 from utils.custom_web_element import CustomWebElement
 
 
@@ -17,10 +18,15 @@ class EventPage(BasePage):
     locators = {
         "main_header_locator": (By.CSS_SELECTOR, ".top-header>.main-header"),
         "create_event_button": (By.CSS_SELECTOR, "div.create button.m-btn"),
+        "event_card": (By.CSS_SELECTOR, "mat-card.event-list-item")
     }
+
+    _event_card_pattern = ("//mat-card[contains(@class, 'event-list-item')]"
+                           "[.//p[contains(@class, 'event-name') and normalize-space()='{}']]")
 
     main_header_locator: CustomWebElement
     create_event_button: CustomWebElement
+    event_card: CustomWebElement
 
     @allure.step("Navigating to the Places page")
     def go_to_places(self) -> "PlacesPage":
@@ -39,3 +45,14 @@ class EventPage(BasePage):
         self.get_wait().until(EC.url_contains("create"))
 
         return CreateEventPage(self.driver)
+
+    def get_event_card_by_name(self, name) -> EventCardComponent:
+        """Finds an event card element by the event's name."""
+        formatted_pattern = self._event_card_pattern.format(name)
+        event_card_element = self.driver.find_element(By.XPATH, formatted_pattern)
+        return EventCardComponent(event_card_element)
+
+    def is_loaded(self):
+        """Checks that Event Page is loaded by event card presence."""
+        event_card_locator = self.locators["event_card"][:2]
+        return self._is_loaded_indicator(event_card_locator)
