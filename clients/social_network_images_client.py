5b9a6e761b1012e10494757390357ef864d33050
Anzhela <anzhela.en.mal@gmail.com>
2026-03-25 15:35:00 +0200

    Added test for verifying getting all social network images is unauthorized without token

---
+++ clients/social_network_images_client.py
@@ -0,0 +1,44 @@
+"""Client for social network images requests"""
+from clients.base_client import BaseClient
+
+
+class SocialNetworkImagesClient(BaseClient):
+    """Client for social network images requests"""
+
+    def __init__(self, base_url, access_token=None):
+        """Initialize social network images client"""
+        super().__init__(
+            base_url=f"{base_url}/management/socialnetworkimages",
+            access_token=access_token
+        )
+
+    def get_all_social_network_images(self):
+        """Get all social network images"""
+        return self._request(
+            method="GET",
+            endpoint=""
+        )
+
+    def create_social_network_image(self, body):
+        """Create social network image"""
+        return self._request(
+            method="POST",
+            endpoint="/",
+            json=body
+        )
+
+    def update_social_network_image(self, body):
+        """Update social network image"""
+        return self._request(
+            method="PUT",
+            endpoint="/",
+            json=body
+        )
+
+    def delete_social_network_image(self, image_id):
+        """Delete social network image"""
+        return self._request(
+            method="DELETE",
+            endpoint="/delete",
+            params={"id": image_id}
+        )
