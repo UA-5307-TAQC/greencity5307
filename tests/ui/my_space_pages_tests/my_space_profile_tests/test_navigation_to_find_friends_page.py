0f7c4c84ada5d31faa108b9a9116a746171e3554
Liubov Titova <titova.l7789@gmail.com>
2026-03-26 13:41:04 +0200

    fix

--- tests/ui/my_space_pages_tests/my_space_profile_tests/test_navigation_to_find_friends_page.py
+++ tests/ui/my_space_pages_tests/my_space_profile_tests/test_navigation_to_find_friends_page.py
@@ -3,9 +3,9 @@ import pytest
 
 from pages.abstract_pages.my_space_abstract.my_habit_page import MyHabitPage
 
-@allure.title("Check that user is redirected to Friends page")
-def test_navigation_to_friends_page(driver_with_login):
-    """Test correct navigation to Find Friend page."""
+@allure.title("Check that user is redirected to Find Friends page")
+def test_navigation_to_find_friends_page(driver_with_login):
+    """Test correct navigation to Find Friends page."""
 
     with allure.step("Go to My Space page"):
         driver = driver_with_login
@@ -14,10 +14,10 @@ def test_navigation_to_friends_page(driver_with_login):
     profile_banner = my_habit_page.profile_banner
     if profile_banner.friends_images_exist():
         pytest.skip("Skip test because there is no [Add friends]")
-    else:
-        with allure.step("Click [Add friends] if user has no friends"):
-            profile_banner.click_add_friends_btn()
 
-        with allure.step("Check if user is redirected to Find Friend page"):
-            url = driver.current_url
-            assert "/friends/recommended" in url, f"Expected to be on Find Friend page, but was at: {url}"
+    with allure.step("Click [Add friends] if user has no friends"):
+        profile_banner.click_add_friends_btn()
+
+    with allure.step("Check if user is redirected to Find Friend page"):
+        url = driver.current_url
+        assert "/friends/recommended" in url, f"Expected to be on Find Friend page, but was at: {url}"
