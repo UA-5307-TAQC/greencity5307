82e02c774a76854b363dff0cf853f02fc03288b3
Anzhela <anzhela.en.mal@gmail.com>
2026-03-26 17:32:50 +0200

    Fix social network images client methods

--- clients/social_network_images_client.py
+++ clients/social_network_images_client.py
@@ -3,40 +3,41 @@ from clients.base_client import BaseClient
 
 
 class SocialNetworkImagesClient(BaseClient):
-    """Client for social network images requests"""
+    """Client for social network images requests."""
 
     def __init__(self, base_url, access_token=None):
-        """Initialize social network images client"""
+        """Initialize social network images client."""
         super().__init__(
             base_url=f"{base_url}/management/socialnetworkimages",
             access_token=access_token
         )
 
     def get_all_social_network_images(self):
-        return self.request(
+        """Get all social network images."""
+        return self._request(
             method="GET",
             endpoint=""
         )
 
-
     def create_social_network_image(self, body):
-        return self.request(
+        """Create social network image."""
+        return self._request(
             method="POST",
             endpoint="/",
             json=body
         )
 
-
     def update_social_network_image(self, body):
-        return self.request(
+        """Update social network image."""
+        return self._request(
             method="PUT",
             endpoint="/",
             json=body
         )
 
-
     def delete_social_network_image(self, image_id):
-        return self.request(
+        """Delete social network image."""
+        return self._request(
             method="DELETE",
             endpoint="/delete",
             params={"id": image_id}
