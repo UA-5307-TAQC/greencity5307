8a8850e2e55e689480f75a6ffc4e8b1e38d69cba
Liubomyr Halamaha <galamagal86@gmail.com>
2026-04-02 13:11:04 +0300

    fix

--- tests/bdd/features/steps/api/google_security/google_security.py
+++ tests/bdd/features/steps/api/google_security/google_security.py
@@ -24,12 +24,6 @@ def step_send_google_request(context, project_name, lang):
     """Send GET request to Google Security API with specified project name and language."""
     context.response = context.client.get_google_security(project_name, lang)
 
-@then('the response status code should be {status_code}')
-def step_validate_status(context, status_code):
-    """Validate that the response status code matches the expected status code."""
-    assert context.response.status_code == int(status_code), context.response.text
-
-
 @then('the response should match google security schema if status is 200')
 def step_validate_schema(context):
     """Validate that the response JSON matches the Google Security schema if the status code is 200."""
@@ -43,17 +37,3 @@ def step_validate_schema(context):
     except ValidationError as e:
         allure.attach(str(e), name="Validation Error",
                       attachment_type=allure.attachment_type.TEXT)
-
-@then('the response should contain "{message_type}" with value "{message}"')
-def step_validate_message(context, message_type, message):
-    """Validate that the response contains the specified message type with the expected value."""
-    if not message_type:  # skip if empty
-        return
-
-    parsed_data = context.response.json()
-
-    assert message_type in parsed_data, \
-        f"Field '{message_type}' not found in response: {parsed_data}"
-
-    assert parsed_data[message_type] == message, \
-        f"Expected '{message}', got '{parsed_data[message_type]}'"
